a7f941a56d23a91c4adf54838d1061ec3781210a
Shweta1319 <47740471+Shweta1319@users.noreply.github.com>
2020-01-18 15:28:03 +0530

    Create Jenkinsfile.sh

---
+++ Jenkinsfile.sh
@@ -0,0 +1,42 @@
+pipeline {
+
+    agent {
+      label "master"
+     }
+          
+    tools {
+       maven 'Maven' 
+       jdk 'Java'      
+    }
+    
+    stages {
+            stage('Build') {
+                    steps {
+                        sh 'mvn install'
+                    }
+           }
+           
+           stage('Test'){
+                    steps{
+                      sh 'mvn test'
+                    }
+                    post {
+                        always {
+                            junit 'target/surefire-reports/*.xml'
+                        }
+                    }
+             }
+             
+            stage('DeploytoNexus'){
+                    steps{
+                      sh 'mvn deploy'
+                    }
+             }
+             
+             stage('DeploytoTomcat'){
+                    steps{
+                        deploy adapters: [tomcat8(credentialsId: '1de8381f-0df6-4c36-99af-467ac731a2a7', path: '', url: 'http://18.222.185.192:9090/')], contextPath: 'banerwar', war: '**/*.war'                    }
+             }
+
+    }
+}
